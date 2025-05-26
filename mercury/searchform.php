e0df43da388a0e42429e2876aa0ee1b757ab475a
dave224 <jindrle@brilo.cz>
2025-05-26 13:42:43 +0200

    pl translations

--- mercury/searchform.php
+++ mercury/searchform.php
@@ -1,3 +1,3 @@
 		<form role="search" method="get" class="space-default-search-form" action="<?php echo esc_url( home_url( '/' ) ) ?>">
-			<input type="search" value="<?php echo get_search_query() ?>" name="s" placeholder="<?php echo esc_attr_x( 'Enter Keyword...', 'placeholder', 'mercury' ); ?>">
+			<input type="search" value="<?php echo get_search_query() ?>" name="s" placeholder="<?php echo esc_html_e( 'Enter keyword...', 'mercury' ); ?>">
 		</form>

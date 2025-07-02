d7959ca1198a8fb42d277996e79c15fb125f2b0b
dave224 <jindrle@brilo.cz>
2025-07-02 08:54:59 +0200

    Simplesio api :: upload image to WP

--- mercury/inc/fourcrowns-fields/theme-functions/simplessio-api.php
+++ mercury/inc/fourcrowns-fields/theme-functions/simplessio-api.php
@@ -36,11 +36,11 @@ function createPostFromApp ($data)
         $author_id = $user->ID;
     }
 
-    $new_html = upload_images_and_replace_urls($postContentData['contentHTML'], 0);
+   // $new_html = upload_images_and_replace_urls($postContentData['contentHTML'], 0);
 
     $my_post = [
         'post_title'    => $postContentData['title'],
-        'post_content'  => $new_html,
+        'post_content'  => $postContentData['contentHTML'],
         'post_excerpt'  => $postContentData['description'],
         'post_status'   => 'draft',
         'post_author'   => $author_id,
@@ -72,7 +72,7 @@ function createPostFromApp ($data)
         $my_post = [
             'ID'            => $posts[0]->ID,
             'post_title'    => $postContentData['title'],
-            'post_content'  => $new_html,
+            'post_content'  => $postContentData['contentHTML'],
             'post_excerpt'  => $postContentData['description'],
             'post_status'   => get_post_status($posts[0]->ID),
             'post_author'   => $author_id,

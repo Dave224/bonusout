ae5a5cd9c3d1bceed0ee9af85058d8863699c3e8
dave224 <jindrle@brilo.cz>
2025-07-03 11:22:16 +0200

    Change web colors

--- mercury/inc/fourcrowns-fields/include.php
+++ mercury/inc/fourcrowns-fields/include.php
@@ -1,6 +1,5 @@
 <?php
-add_action( 'wp_enqueue_scripts', 'four_crowns_enqueue_styles', 20 );
-
+add_action( 'wp_enqueue_scripts', 'four_crowns_enqueue_styles', 30 );
 
 function four_crowns_enqueue_styles()
 {
@@ -9,7 +8,7 @@ function four_crowns_enqueue_styles()
     wp_enqueue_script('swiper-js', 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js', array(), null, true);
 
     // Custom css + js
-    wp_enqueue_style('custom_theme_css', get_theme_file_uri(FOUR_CROWNS_ASSETS . '/css/custom-style.css'), ['mercury-style-css'], '2025-07-03');
+    wp_enqueue_style('custom_theme_css', get_theme_file_uri(FOUR_CROWNS_ASSETS . '/css/custom-style.css'), [], '2025-07-03-2');
     wp_enqueue_script('custom_scripts_js', get_theme_file_uri(FOUR_CROWNS_ASSETS . '/js/custom-scripts.js'), [], '2025-06-27', true);
 
     // Honeypot

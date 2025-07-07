e94682a6288813a8fa54f18fdbb2995bc1dc953a
dave224 <jindrle@brilo.cz>
2025-07-07 11:36:02 +0200

    add top brands to main settings

--- mercury/inc/fourcrowns-fields/theme-parts/top-brands.php
+++ mercury/inc/fourcrowns-fields/theme-parts/top-brands.php
@@ -12,7 +12,7 @@ if ($mainSettings) {
 }
 ;
 $key = 1;
-var_dump($topBrands);
+var_dump($topBrands && $topBrands != []);
 ?>
 <?php if ($topBrands && $topBrands != []) { ?>
     <div class="custom-wrapper custom-category-brands-wrapper relative">

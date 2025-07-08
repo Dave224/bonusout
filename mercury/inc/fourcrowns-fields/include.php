223a6f7e200eac8cf74aaaf4911a767c45f94645
dave224 <jindrle@brilo.cz>
2025-07-08 08:32:21 +0200

    Logo max with settings

--- mercury/inc/fourcrowns-fields/include.php
+++ mercury/inc/fourcrowns-fields/include.php
@@ -8,7 +8,7 @@ function four_crowns_enqueue_styles()
     wp_enqueue_script('swiper-js', 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js', array(), null, true);
 
     // Custom css + js
-    wp_enqueue_style('custom_theme_css', get_theme_file_uri(FOUR_CROWNS_ASSETS . '/css/custom-style.css'), [], '2025-07-08');
+    wp_enqueue_style('custom_theme_css', get_theme_file_uri(FOUR_CROWNS_ASSETS . '/css/custom-style.css'), [], '2025-07-08-2');
     wp_enqueue_script('custom_scripts_js', get_theme_file_uri(FOUR_CROWNS_ASSETS . '/js/custom-scripts.js'), [], '2025-06-27', true);
 
     // Honeypot

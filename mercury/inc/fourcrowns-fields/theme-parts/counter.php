e9d5f0bc80765c2bace237bac8c929d0582dcb6d
dave224 <jindrle@brilo.cz>
2025-07-28 10:15:27 +0200

    Counter :: add condition for whole section

--- mercury/inc/fourcrowns-fields/theme-parts/counter.php
+++ mercury/inc/fourcrowns-fields/theme-parts/counter.php
@@ -8,30 +8,32 @@ $under_description = Fourcrowns_Storage::get('post', get_queried_object_id(), CO
 $numbers = Fourcrowns_Storage::get('post', get_queried_object_id(), COUNTER);
 ?>
 
-<div class="custom-wrapper relative <?= $background; ?>">
-    <?php if (Util::issetAndNotEmpty($title) || Util::issetAndNotEmpty($description)) { ?>
-        <div class="space-page-wrapper relative space-page-content box-100">
-            <h2><?= $title; ?></h2>
-            <?= $description; ?>
-        </div>
-    <?php } ?>
+<?php if (is_array($numbers) || Util::issetAndNotEmpty($title) || Util::issetAndNotEmpty($description)) { ?>
+    <div class="custom-wrapper relative <?= $background; ?>">
+        <?php if (Util::issetAndNotEmpty($title) || Util::issetAndNotEmpty($description)) { ?>
+            <div class="space-page-wrapper relative space-page-content box-100">
+                <h2><?= $title; ?></h2>
+                <?= $description; ?>
+            </div>
+        <?php } ?>
 
-     <?php if (is_array($numbers) && Util::issetAndNotEmpty($numbers[0][COUNTER . '_number'])) { ?>
-         <div class="space-page-wrapper relative">
-             <div class="counter-grid">
-                 <?php foreach ( $numbers as $counter ) : ?>
-                     <div class="counter-item">
-                         <span class="counter-number" data-target="<?= $counter[COUNTER . '_number']; ?>">0</span>
-                         <span class="counter-label"><?= $counter[COUNTER . '_title']; ?></span>
-                     </div>
-                 <?php endforeach; ?>
+         <?php if (is_array($numbers) && Util::issetAndNotEmpty($numbers[0][COUNTER . '_number'])) { ?>
+             <div class="space-page-wrapper relative">
+                 <div class="counter-grid">
+                     <?php foreach ( $numbers as $counter ) : ?>
+                         <div class="counter-item">
+                             <span class="counter-number" data-target="<?= $counter[COUNTER . '_number']; ?>">0</span>
+                             <span class="counter-label"><?= $counter[COUNTER . '_title']; ?></span>
+                         </div>
+                     <?php endforeach; ?>
+                 </div>
              </div>
-         </div>
-     <?php } ?>
+         <?php } ?>
 
-    <?php if ($under_description) { ?>
-        <div class="space-page-wrapper relative space-page-content box-100">
-            <?= $under_description; ?>
-        </div>
-    <?php } ?>
-</div>
+        <?php if ($under_description) { ?>
+            <div class="space-page-wrapper relative space-page-content box-100">
+                <?= $under_description; ?>
+            </div>
+        <?php } ?>
+    </div>
+<?php } ?>

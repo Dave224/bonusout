785f518e7aeba018a6b3716a700f2474f1c8e47e
dave224 <jindrle@brilo.cz>
2025-08-20 09:22:41 +0200

    Page Ooops :: implementation

---
+++ mercury/page-templates/page-ooops.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Template Name: Nenalezený brand
+*/
+global $post;
+?>
+<?php get_header(); ?>
+
+    <div class="custom-wrapper relative">
+        <div class="space-page-wrapper relative space-page-content box-100 hp-top-text">
+            <h1 class="main-title"><?= get_the_title($post->ID); ?></h1>
+            <?= apply_filters('the_content', $post->post_content); ?>
+        </div>
+    </div>
+
+<?php get_template_part( FOUR_CROWNS_THEME_PARTS . '/listing' ); ?>
+
+<?php get_footer(); ?>

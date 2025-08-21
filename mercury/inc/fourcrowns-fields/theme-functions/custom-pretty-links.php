5e9100b8572060580d335028d900cc878b592db3
dave224 <jindrle@brilo.cz>
2025-08-21 08:37:58 +0200

    PrettyLinks :: redirection collection connect

--- mercury/inc/fourcrowns-fields/theme-functions/custom-pretty-links.php
+++ mercury/inc/fourcrowns-fields/theme-functions/custom-pretty-links.php
@@ -118,6 +118,37 @@ if (str_contains($current_url, '/go/')) {
             }
         }
 
+        $collectionUrl = 'https://app.simplesio.com/api/redirections';
+
+        // Vytvoření parametrů pro Payload query
+        $params = [
+            'where[from][equals]' => '/go' . $url_array_for_match[1],
+        ];
+
+        // Sestavení query stringu
+        $queryString = http_build_query($params);
+
+        $requestUrl = $collectionUrl . '?' . $queryString;
+
+        $ch = curl_init($requestUrl);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            'Authorization: Bearer ' . $token,
+            'Content-Type: application/json'
+        ]);
+        $response = curl_exec($ch);
+        curl_close($ch);
+
+        // Výpis nebo další zpracování dat
+        $data = json_decode($response, true);
+        $link = $data['docs'][0]["to"];
+
+        if ($data && $link) {
+            wp_redirect($link);
+            die;
+        }
+
+        // Přesměrování na WP stránku v případě že ani jedno z výše uvedených pravidel v aplikaci není platné
         $template = 'page-templates/page-ooops.php'; // relativně k rootu šablony
         $pages = get_pages([
             'post_type'      => 'page',
@@ -138,6 +169,24 @@ if (str_contains($current_url, '/go/')) {
 
         wp_die("Affiliate brand nebyl nalezen!");
     } else {
+        $template = 'page-templates/page-ooops.php'; // relativně k rootu šablony
+        $pages = get_pages([
+            'post_type'      => 'page',
+            'post_status'    => 'publish',
+            'meta_key'       => '_wp_page_template',
+            'meta_value'     => $template,
+            'number'         => 1,  // vezmeme jen první
+            'hierarchical'   => 0,
+        ]);
+
+
+
+        if (!empty($pages)) {
+            $page_id = $pages[0]->ID;
+            $url = get_permalink($page_id);
+            wp_redirect($url);die;
+        }
+
         wp_die("Chyba při přihlášení: " . $response);
     }
 }

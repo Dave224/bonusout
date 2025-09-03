c7439a3de1781d1eebeb7a9d850791d0120471ef
dave224 <jindrle@brilo.cz>
2025-09-03 09:58:46 +0200

    rename framework

---
+++ mercury/inc/custom-fields/projectContants.php
@@ -0,0 +1,4 @@
+<?php
+const FOUR_CROWNS_INC = '/inc/custom-fields';
+const FOUR_CROWNS_ASSETS = '/inc/custom-fields/assets';
+const FOUR_CROWNS_THEME_PARTS = '/inc/custom-fields/theme-parts';
--- mercury/inc/fourcrowns-fields/projectContants.php
+++
@@ -1,4 +0,0 @@
-<?php
-const FOUR_CROWNS_INC = '/inc/fourcrowns-fields';
-const FOUR_CROWNS_ASSETS = '/inc/fourcrowns-fields/assets';
-const FOUR_CROWNS_THEME_PARTS = '/inc/fourcrowns-fields/theme-parts';

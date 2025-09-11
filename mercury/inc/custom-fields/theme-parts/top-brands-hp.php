c5abd65a16d043cb702536cb9dc4d4fa7d71ea4a
dave224 <jindrle@brilo.cz>
2025-09-11 09:14:11 +0200

    TopBrands :: margin on mobile fix

--- mercury/inc/custom-fields/theme-parts/top-brands-hp.php
+++ mercury/inc/custom-fields/theme-parts/top-brands-hp.php
@@ -16,7 +16,7 @@ $key = 1;
 
 <?php if ($mainTitle || $mainDescription) { ?>
     <div class="custom-wrapper relative">
-        <div class="space-page-wrapper relative space-page-content box-100 hp-top-text">
+        <div class="space-page-wrapper relative space-page-content hp-top-text">
             <h1 class="main-title"><?= $mainTitle; ?></h1>
             <?= $mainDescription; ?>
         </div>

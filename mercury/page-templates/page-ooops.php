c7e22bcce85ebd3665355e81ad4384f15cb8ccc6
dave224 <jindrle@brilo.cz>
2025-09-19 11:09:20 +0200

    PageOoops :: mobile spaces fix

--- mercury/page-templates/page-ooops.php
+++ mercury/page-templates/page-ooops.php
@@ -7,7 +7,7 @@ global $post;
 <?php get_header(); ?>
 
     <div class="custom-wrapper relative">
-        <div class="space-page-wrapper relative space-page-content box-100 hp-top-text">
+        <div class="space-page-wrapper relative space-page-content hp-top-text">
             <h1 class="main-title"><?= get_the_title($post->ID); ?></h1>
             <?= apply_filters('the_content', $post->post_content); ?>
         </div>

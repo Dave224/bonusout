b907a693b03e44ddd141eacb770742c4b6a66c72
dave224 <jindrle@brilo.cz>
2025-09-30 09:42:10 +0200

    SchemaGenerator :: implementation

--- mercury/header.php
+++ mercury/header.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 <?= Fourcrowns_Storage::get('option', null, 'custom-settings-header-code'); ?>
     <?php if (is_single()) {
-        require_once(FOUR_CROWNS_THEME_FUNCTIONS . 'SchemaGenerator.php'); ?>
+        require_once(FOUR_CROWNS_THEME_FUNCTIONS . '/SchemaGenerator.php'); ?>
     <?php } ?>
 </head>
 <body ontouchstart <?php body_class(); ?>>

dc3f4d8d4b534325744a9f04b2124fa1408794e1
dave224 <jindrle@brilo.cz>
2025-10-08 09:17:41 +0200

    Add page thanks

---
+++ mercury/page-templates/page-thanks.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Template Name: Děkovací stránka
+*/
+global $post;
+?>
+<?php get_header(); ?>
+
+    <div class="custom-wrapper relative">
+        <div class="space-page-wrapper relative space-page-content hp-top-text">
+            <h1 class="main-title"><?= get_the_title($post->ID); ?></h1>
+            <?= apply_filters('the_content', $post->post_content); ?>
+        </div>
+    </div>
+
+<?php get_template_part( FOUR_CROWNS_THEME_PARTS . '/listing' ); ?>
+
+<?php get_footer(); ?>
